b7a86739dfff3b31260e6bea98c1689d35b32879
TaehwanKim <dmkthwan11@gmail.com>
2023-12-22 19:27:46 +0900

    add history box

--- repheat/lib/Statistics.dart
+++ repheat/lib/Statistics.dart
@@ -3,6 +3,8 @@ import 'package:intl/intl.dart';
 import 'ColorScheme.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+import 'TimerList.dart';
+
 class Statistics extends StatelessWidget {
   Map<String, dynamic> history;
   Statistics({required this.history});
@@ -33,6 +35,7 @@ class TableCalendarScreen extends StatefulWidget {
 
 class _TableCalendarScreenState extends State<TableCalendarScreen> {
   DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   int _numberOfWeeksInMonth(DateTime date) {
     // Find the first day of the month
@@ -60,155 +63,210 @@ class _TableCalendarScreenState extends State<TableCalendarScreen> {
   Widget build(BuildContext context) {
     GlobalKey _calendarKey = GlobalKey();
     int numberOfWeeks = _numberOfWeeksInMonth(_focusedDay);
-    double calendarHeight = (numberOfWeeks == 6) ? 315.0 : 280.0;
+    double calendarHeight = (numberOfWeeks == 6) ? 290.0 : 250.0;
+    var log;
+    if (_selectedDay != null) {
+      log = widget.history[_selectedDay!.year.toString()]?[_selectedDay!.month.toString()]?[_selectedDay!.day.toString()];
+      // Ensure the log is a list before proceeding.
+      if (log is! List) {
+        log = [];  // Default to an empty list if it's not a list.
+      }
+      // Rest of your code that depends on _selectedDay being not null...
+    }
+    int? selectedBoxIndex;
     return Container(
-      width: 263,
-      height: calendarHeight,
-      child: Column(
-        children: [
-          Expanded(
-            child: TableCalendar(
-              key: _calendarKey,
-              shouldFillViewport: true,
-              locale: 'ko-KR',
-              firstDay: DateTime.utc(2000, 1, 1),
-              lastDay: DateTime.utc(2030, 12, 31),
-              onPageChanged: (focusedDay) {
-                setState(() {
-                  _focusedDay = focusedDay;
-                  // Reassign the key to force a rebuild
-                  _calendarKey = GlobalKey();
-                });
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                if (!isSameDay(_focusedDay, focusedDay)) {
-                  setState(() {
-                    _focusedDay = focusedDay;
-                  });
-                }
-              },
-              focusedDay: _focusedDay,
-              calendarBuilders: CalendarBuilders(
-                defaultBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, false),
-                todayBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, true),
-                outsideBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, false),
-                disabledBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, false),
-                dowBuilder: (context, day) {
-                  return Center(child: Text(''));
-                },
+        child: SingleChildScrollView(
+
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+
+            children: [
+              SizedBox(
+                width: 263,
+                height: calendarHeight,
+                child: TableCalendar(
+                  key: _calendarKey,
+                  shouldFillViewport: true,
+                  locale: 'ko-KR',
+                  firstDay: DateTime.utc(2000, 1, 1),
+                  lastDay: DateTime.utc(2030, 12, 31),
+                  onPageChanged: (focusedDay) {
+                    setState(() {
+                      _focusedDay = focusedDay;
+                      // Reassign the key to force a rebuild
+                      _calendarKey = GlobalKey();
+                    });
+                  },
+                  onDaySelected: (selectedDay, focusedDay) {
+                    if (!isSameDay(_focusedDay, focusedDay)) {
+                      setState(() {
+                        _focusedDay = focusedDay;
+                        _selectedDay = selectedDay;
+                      });
+                    }
+                  },
+                  focusedDay: _focusedDay,
+                  calendarBuilders: CalendarBuilders(
+                    defaultBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, false),
+                    todayBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, true),
+                    outsideBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, false),
+                    disabledBuilder: (context, day, focusedDay) => cellBuilder(context, day, focusedDay, false),
+                    dowBuilder: (context, day) {
+                      return Center(child: Text(''));
+                    },
+                  ),
+
+                  headerStyle: HeaderStyle(
+                      titleCentered: true,
+                      titleTextFormatter: (date, locale) => DateFormat('yyyy.MM').format(date),
+                      formatButtonVisible: false,
+                      titleTextStyle: TextStyle(
+                        fontSize: 30.0,
+                        color: ColorSet['white'],
+                      ),
+                      leftChevronIcon: Center(
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          alignment: Alignment.center, // This centers the icon within the container
+                          decoration: BoxDecoration(
+                            color: ColorSet['black'],
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: Icon(Icons.chevron_left, color: ColorSet['white'], size: 15,),
+                        ),
+                      ),
+                      leftChevronPadding: EdgeInsets.all(0),
+                      rightChevronIcon: Center(
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          alignment: Alignment.center, // This centers the icon within the container
+                          decoration: BoxDecoration(
+                            color: ColorSet['black'],
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: Icon(Icons.chevron_right, color: ColorSet['white'], size: 15,),
+                        ),
+                      ),
+                      rightChevronPadding: EdgeInsets.all(0)
+                  ),
+                ),
               ),
 
-              headerStyle: HeaderStyle(
-                  titleCentered: true,
-                  titleTextFormatter: (date, locale) => DateFormat('yyyy.MM').format(date),
-                  formatButtonVisible: false,
-                  titleTextStyle: TextStyle(
-                    fontSize: 30.0,
-                    color: ColorSet['white'],
-                  ),
-                  leftChevronIcon: Center(
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      alignment: Alignment.center, // This centers the icon within the container
-                      decoration: BoxDecoration(
-                        color: ColorSet['black'],
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                      child: Icon(Icons.chevron_left, color: ColorSet['white'], size: 15,),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100, 2.0, 0, 2.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text('Less', style: TextStyle(color: ColorSet['white'], fontSize: 12),),
                     ),
-                  ),
-                  leftChevronPadding: EdgeInsets.all(0),
-                  rightChevronIcon: Center(
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      alignment: Alignment.center, // This centers the icon within the container
-                      decoration: BoxDecoration(
-                        color: ColorSet['black'],
-                        borderRadius: BorderRadius.circular(4),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          color: Gradient1['0'],
+                          borderRadius: BorderRadius.circular(3),
+                        ),
                       ),
-                      child: Icon(Icons.chevron_right, color: ColorSet['white'], size: 15,),
                     ),
-                  ),
-                  rightChevronPadding: EdgeInsets.all(0)
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          color: Gradient1['1'],
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          color: Gradient1['2'],
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          color: Gradient1['3'],
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Container(
+                        width: 15,
+                        height: 15,
+                        decoration: BoxDecoration(
+                          color: Gradient1['4'],
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text('More', style: TextStyle(color: ColorSet['white'], fontSize: 12),),
+                    ),
+                  ],
+                ),
               ),
-            ),
+
+              if (_selectedDay != null)
+                Container(
+                  // Your widget to display below the calendar
+                    child: SingleChildScrollView( // Wrap with SingleChildScrollView
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: List<Widget>.generate(log.length, (index) {
+                            var routine = log[index];
+                            var subroutines = routine['subroutines'] as List<dynamic>;
+                            TimerListState util = TimerListState();
+                            return Column(
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedBoxIndex = selectedBoxIndex == index ? null : index;
+                                    });
+                                  },
+                                  child: (selectedBoxIndex != index)
+                                      ? util.bigBox(context, routine, false, index)
+                                      : util.bigBox(context, routine, true, index),
+                                ),
+                                if (selectedBoxIndex == index) ...subroutines.map((sub) {
+                                  return util.littleBox(context, sub);
+                                }).toList(),
+                              ],
+                            );
+                          }),
+                        ),
+                      ),
+                    )
+
+
+                  // Text("Details for ${widget.history[_selectedDay!.year.toString()]?[_selectedDay!.month.toString()]?[_selectedDay!.day.toString()]}"),
+
+                  // Implement more complex logic or additional widgets here
+                ),
+            ],
           ),
-
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Text('Less', style: TextStyle(color: ColorSet['white'], fontSize: 12),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Container(
-                    width: 15,
-                    height: 15,
-                    decoration: BoxDecoration(
-                      color: Gradient1['0'],
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Container(
-                    width: 15,
-                    height: 15,
-                    decoration: BoxDecoration(
-                      color: Gradient1['1'],
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Container(
-                    width: 15,
-                    height: 15,
-                    decoration: BoxDecoration(
-                      color: Gradient1['2'],
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Container(
-                    width: 15,
-                    height: 15,
-                    decoration: BoxDecoration(
-                      color: Gradient1['3'],
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Container(
-                    width: 15,
-                    height: 15,
-                    decoration: BoxDecoration(
-                      color: Gradient1['4'],
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: Text('More', style: TextStyle(color: ColorSet['white'], fontSize: 12),),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+        )
     );
   }
 
@@ -218,7 +276,13 @@ class _TableCalendarScreenState extends State<TableCalendarScreen> {
     String dayOfMonth = day.day.toString();
 
     // Determine the styling based on user history
-    int? userActivity = widget.history[year]?[month]?[dayOfMonth];
+    var log = widget.history[year]?[month]?[dayOfMonth];
+    // Ensure the log is a list before proceeding.
+    if (log is! List) {
+      log = [];  // Default to an empty list if it's not a list.
+    }
+
+    int userActivity = log.length;
 
     Border? border = (isToday) ? Border.all(
         color: ColorSet['white']!,
